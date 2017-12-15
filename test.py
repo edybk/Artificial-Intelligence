a88a02c45a5fcdde3ce5d2b923ff862ba4ec1181
Eddie Bkheet <edy.bkheet@gmail.com>
2017-12-15 20:25:49 +0200

    implementing better-player

--- test.py
+++ test.py
@@ -1,20 +1,27 @@
 from run_game import GameRunner
 
+import abstract
+from utils import INFINITY, run_with_limited_time, ExceededTimeError
+from Reversi.consts import *
+import time
+import copy
 verbose = "n"
-x_player = "random_player"
+x_player = "better_player"
 o_player = "simple_player"
 k = 5
-time_per_k_turns = 10
-setup_time = 1
+time_per_k_turns = 100
+setup_time = 20
 
 runner_x_first = GameRunner(setup_time, time_per_k_turns, k, verbose, x_player, o_player)
 
 runner_o_first = GameRunner(setup_time, time_per_k_turns, k, verbose, o_player, x_player)
 
-for round in range(3):
-    runner_x_first.run()
-for round in range(3):
-    runner_o_first.run()
+runner_o_first.run()
 
-print("hey")
+#print("X random starts 3 times:")
+#for round in range(3):
+#    runner_x_first.run()
 
+#print("X simple starts 3 times:")
+#for round in range(3):
+#    runner_o_first.run()
